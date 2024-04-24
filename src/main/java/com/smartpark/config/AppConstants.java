23a45c7579d3ce208398965a8e184b67235bf55d
Sunil Singh Tarkar <sunilthakur89544@gmail.com>
2024-04-24 11:38:47 +0530

    Kafka and sftp example done

--- src/main/java/com/smartpark/config/AppConstants.java
+++ src/main/java/com/smartpark/config/AppConstants.java
@@ -2,6 +2,9 @@ package com.smartpark.config;
 
 public class AppConstants {
 	public static final String PARKINGSLOT_TOPIC_NAME = "parkingSlot-update-topic";
+	public static final String VEHICLE_TOPIC_NAME = "vehicle-topic";
+	public static final String PARK_TOPIC_NAME = "parkingLot-activity-topic";
+
 	public static final String BOOTSTRAP_SERVERS = "localhost:9092";
 
 }
--- src/main/java/com/smartpark/contoller/ParkingController.java
+++ src/main/java/com/smartpark/contoller/ParkingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,7 +62,7 @@ public class ParkingController {
 	}
 
 	// Endpoint to empty a slot
-	@PostMapping("/emptySlot/{slotNum}")
+	@PutMapping("/emptySlot/{slotNum}")
 	public ResponseEntity<String> emptySlot(@PathVariable Integer slotNum) {
 		String response = parkingSlotService.emptySlot(slotNum);
 		return ResponseEntity.ok(response);
